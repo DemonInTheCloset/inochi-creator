023a44c937bee57f3a88c43de5998de213bafd27
DemonInTheCloset <demoninthecloset@gmail.com>
2022-07-17 18:11:14 +0200

    feat: Slightly more split psd importer

--- source/creator/io/psd.d
+++ source/creator/io/psd.d
@@ -15,56 +15,78 @@ import psd;
 import i18n;
 import std.format;
 
-private {
+private
+{
+    BlendMode blendModeKeyToBlendingMode(BlendingMode blendModeKey)
+    {
+        switch (blendModeKey)
+        {
+        case BlendingMode.Multiply:
+            return BlendMode.Multiply;
+        case BlendingMode.LinearDodge:
+            return BlendMode.LinearDodge;
+        case BlendingMode.ColorDodge:
+            return BlendMode.ColorDodge;
+        case BlendingMode.Screen:
+            return BlendMode.Screen;
+        default:
+            return BlendMode.Normal;
+        }
+    }
 
-}
-
-/**
-    Imports a PSD file.
-*/
-void incImportPSD(string file) {
-    incNewProject();
-    // TODO: Split this up to a seperate file and make it cleaner
-    try {
-        import psd : PSD, Layer, LayerType, LayerFlags, parseDocument, BlendingMode;
+    Puppet _incImportPSD(PSD doc)
+    {
+        import psd : PSD, Layer, LayerType, LayerFlags, BlendingMode;
         import std.array : join;
-        PSD doc = parseDocument(file);
-        vec2i docCenter = vec2i(doc.width/2, doc.height/2);
+
         Puppet puppet = new Puppet();
+        vec2i docCenter = vec2i(doc.width / 2, doc.height / 2);
 
         Layer[] layerGroupStack;
-        bool isLastStackItemHidden() {
-            return layerGroupStack.length > 0 ? (layerGroupStack[$-1].flags & LayerFlags.Visible) != 0 : false;
+        bool isLastStackItemHidden()
+        {
+            return layerGroupStack.length > 0
+                ? (layerGroupStack[$ - 1].flags & LayerFlags.Visible) != 0 : false;
         }
 
         string[] path;
         string calcPath;
-        void pushGroupStackName(string layerName) {
+        void pushGroupStackName(string layerName)
+        {
             path ~= layerName;
-            calcPath = "/"~path.join("/");
+            calcPath = "/" ~ path.join("/");
         }
 
-        void popGroupStackName() {
+        void popGroupStackName()
+        {
             path.length--;
-            calcPath = "/"~path.join("/");
+            calcPath = "/" ~ path.join("/");
         }
 
-        foreach_reverse(i, Layer layer; doc.layers) {
+        foreach_reverse (i, Layer layer; doc.layers)
+        {
             import std.stdio : writeln;
+
             debug writeln(layer.name, " ", layer.blendModeKey);
 
             // Skip folders ( for now )
-            if (layer.type != LayerType.Any) {
-                if (layer.name != "</Layer set>" && layer.name != "</Layer group>") {
+            if (layer.type != LayerType.Any)
+            {
+                if (layer.name != "</Layer set>" && layer.name != "</Layer group>")
+                {
                     layerGroupStack ~= layer;
                     pushGroupStackName(layer.name);
-                } else {
+                }
+                else
+                {
                     layerGroupStack.length--;
                     popGroupStackName();
                 }
 
                 continue;
-            } else pushGroupStackName(layer.name);
+            }
+            else
+                pushGroupStackName(layer.name);
 
             layer.extractLayerImage();
             inTexPremultiply(layer.data);
@@ -72,66 +94,61 @@ void incImportPSD(string file) {
             ExPart part = incCreateExPart(tex, puppet.root, layer.name);
             part.layerPath = calcPath;
 
-            auto layerSize = cast(int[2])layer.size();
-            vec2i layerPosition = vec2i(
-                layer.left,
-                layer.top
-            );
+            auto layerSize = cast(int[2]) layer.size();
+            vec2i layerPosition = vec2i(layer.left, layer.top);
 
-            part.localTransform.translation = vec3(
-                (layerPosition.x+(layerSize[0]/2))-docCenter.x,
-                (layerPosition.y+(layerSize[1]/2))-docCenter.y,
-                0
-            );
-
+            part.localTransform.translation = vec3((layerPosition.x + (layerSize[0] / 2)) - docCenter.x,
+                    (layerPosition.y + (layerSize[1] / 2)) - docCenter.y, 0);
 
             part.enabled = (layer.flags & LayerFlags.Visible) == 0;
-            part.opacity = (cast(float)layer.opacity)/255;
-            part.zSort = -(cast(float)i);
-            switch(layer.blendModeKey) {
-                case BlendingMode.Multiply:
-                    part.blendingMode = BlendMode.Multiply; break;
-                case BlendingMode.LinearDodge:
-                    part.blendingMode = BlendMode.LinearDodge; break;
-                case BlendingMode.ColorDodge:
-                    part.blendingMode = BlendMode.ColorDodge; break;
-                case BlendingMode.Screen:
-                    part.blendingMode = BlendMode.Screen; break;
-                default:
-                    part.blendingMode = BlendMode.Normal; break;
-            }
+            part.opacity = (cast(float) layer.opacity) / 255;
+            part.zSort = -(cast(float) i);
+            part.blendingMode = blendModeKeyToBlendingMode(layer.blendModeKey);
             debug writeln(part.name, ": ", part.blendingMode);
 
             // Handle layer stack stuff
-            if (layerGroupStack.length > 0) {
-                if (isLastStackItemHidden()) part.enabled = false;
-                if (layerGroupStack[$-1].blendModeKey != BlendingMode.PassThrough) {
-                    switch(layerGroupStack[$-1].blendModeKey) {
-                        case BlendingMode.Multiply:
-                            part.blendingMode = BlendMode.Multiply; break;
-                        case BlendingMode.LinearDodge:
-                            part.blendingMode = BlendMode.LinearDodge; break;
-                        case BlendingMode.ColorDodge:
-                            part.blendingMode = BlendMode.ColorDodge; break;
-                        case BlendingMode.Screen:
-                            part.blendingMode = BlendMode.Screen; break;
-                        default:
-                            part.blendingMode = BlendMode.Normal; break;
-                    }
-                }
+            if (layerGroupStack.length > 0)
+            {
+                if (isLastStackItemHidden())
+                    part.enabled = false;
+                if (layerGroupStack[$ - 1].blendModeKey != BlendingMode.PassThrough)
+                    part.blendingMode = blendModeKeyToBlendingMode(layerGroupStack[$ - 1].blendModeKey);
             }
 
             puppet.root.addChild(part);
 
-            if (layer.type == LayerType.Any) popGroupStackName();
+            if (layer.type == LayerType.Any)
+                popGroupStackName();
         }
 
         puppet.populateTextureSlots();
+
+        return puppet;
+    }
+}
+
+/**
+    Imports a PSD file.
+*/
+void incImportPSD(string file)
+{
+    incNewProject();
+    // TODO: Split this up to a seperate file and make it cleaner
+    try
+    {
+        import psd : PSD, parseDocument;
+
+        PSD doc = parseDocument(file);
+
+        Puppet puppet = _incImportPSD(doc);
+
         incActiveProject().puppet = puppet;
         incFocusCamera(incActivePuppet().root);
 
         incSetStatus(_("%s was imported...".format(file)));
-    } catch (Exception ex) {
+    }
+    catch (Exception ex)
+    {
 
         incSetStatus(_("Import failed..."));
         incDialog(__("Error"), _("An error occured during PSD import:\n%s").format(ex.msg));
